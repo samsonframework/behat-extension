7c6e93323fbddd3ef8c6e0ba715fd11104461065
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-12-22 18:17:30 +0100

    Added bulk test file

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 22.12.16 at 18:16
+ */
+namespace samsonframework\behatextension\tests;
+
+/**
+ * Class TestCase
+ *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    public function testBulk()
+    {
+        // TODO: Write tests
+        $this->assertTrue(true);
+    }
+}
